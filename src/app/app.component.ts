542b4b1129f2be34e463da7cb68d5b57bfc6f48d
mugishajc_rw <mugishajeanclaude172741@gmail.com>
2022-06-23 09:03:03 +0200

    Dashaboard initial build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TOH';
+  public title = 'TOH Mugisha App';
+  public isAuthenticated: boolean;
+
+  constructor() {
+    this.isAuthenticated = false;
+  }
+
+  login() {
+  }
+
+  logout() {
+  }
 }
